38128e6be57df1c95735be0c372c702306da2ff3
nagasaki-0225 <133938359+nagasaki-0225@users.noreply.github.com>
2023-07-28 02:35:55 +0900

    footerCss

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,15 @@
 <footer class>
+<div class="container">
+<link rel="stylesheet" href="{{ asset('css/footer.css') }}">
+
+    </div>
     <div class="d-flex justify-content-center align-items-center bg-dark fixed-bottom" style="height: 60px; z-index: auto;">
-        <p class="text-white small mb-0">&copy; {{ config('app.name', 'Laravel') }} All rights reserved.</p>
-    </div> 
+{{-- ＃にはリンク先を指定する --}}
+<a href="#" class="example rounded">M</a>
+<a href="#" class="example rounded">D</a>
+<a href="#" class="example rounded">R</a>
+<a href="#" class="example rounded">S</a>
+<br>
+<p class="text-white small mb-0">&copy; {{ config('app.name', 'Laravel') }} All rights reserved.</p>
+</div> 
 </footer>
