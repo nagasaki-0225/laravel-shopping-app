e6c3e2b8a01891ed9a13cfea8a45db941c486dc3
nagasaki-0225 <133938359+nagasaki-0225@users.noreply.github.com>
2023-08-13 13:44:13 +0900

    update

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -22,6 +22,7 @@ class StockController extends Controller
         $stock = new Stock();
         $stock->name = $request->input('name');
         $stock->number = $request->input('number');
+        $stock->item_unit = $request->input('item_unit');
         $stock->shop_name =$request->input('shop_name');
         $stock->price = $request->input('price');
         $stock->user_id = Auth::id();
@@ -30,4 +31,26 @@ class StockController extends Controller
         return redirect()->route('stock.create')->with('flash_message', 'データを登録しました。');
 
     }
+    public function update(Request $request, Stock $stock) {
+    
+        $request->validate([
+            'title' => 'required',
+        ]);
+
+        $stock->name = $request->input('name');
+        $stock->number = $request->input('number');
+        $stock->item_unit = $request->input('item_unit');
+        $stock->shop_name =$request->input('shop_name');
+        $stock->price = $request->input('price');
+        $stock->user_id = Auth::id();
+        $stock->save();
+
+        return redirect()->route('stock.create')>with('flash_message', 'データを更新しました。');      
+   }
+   public function destroy(Stock $stock) {
+    
+    $stock->delete();
+
+    return redirect()->route('stock.create')->with('flash_message', 'データを削除しました。');    
+}
 }
