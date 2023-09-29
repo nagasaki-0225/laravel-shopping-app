276c99d0ee038fa1b2ee1bb9e5a38e5b6131dca4
Taiki Kato <tensainanoda@gmail.com>
2023-09-29 10:42:33 +0900

    Update TopController.php

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -19,48 +19,55 @@ class TopController extends Controller
     }
 
     public function list(Request $request) {
-        $stocks = [];
-        $all_needs_stocks = [];
-    
         if (!$request->has('selected_dishes')) {
             return view('list', ['dishes' => []]);
         }
     
+        $all_needs_stocks = [];
+    
+        // Calculate the total amount needed for each stock
         foreach ($request->selected_dishes as $dish_id => $value) {
             $dish = Dish::find($dish_id);
             $dish->amount = $value['amount'];
     
             foreach ($dish->stocks as $stock) {
                 $needed_stock = $stock->pivot->item_number * $dish->amount;
-                Log::info($stock->id . "が" .$stock->pivot->item_number . "個必要で" .$dish->amount . "前だから" . $needed_stock);
+                $shopName = $stock->shop_name;
     
-                if (array_key_exists($stock->id, $all_needs_stocks)) {
-                    $all_needs_stocks[$stock->id] += $needed_stock;
+                if (!isset($all_needs_stocks[$shopName])) {
+                    $all_needs_stocks[$shopName] = [];
+                }
+    
+                if (array_key_exists($stock->id, $all_needs_stocks[$shopName])) {
+                    $all_needs_stocks[$shopName][$stock->id] += $needed_stock;
                 } else {
-                    $all_needs_stocks[$stock->id] = $needed_stock;
+                    $all_needs_stocks[$shopName][$stock->id] = $needed_stock;
                 }
             }
         }
     
-        // shop_nameでグルーピング
-        foreach ($all_needs_stocks as $stock_id => $needed_amount) {
-            $results = DB::table('stocks')
-                ->select('name',
-                    'shop_name',
-                    DB::raw('count(*) as total'),
-                    'price',
-                    'item_unit',
-                    DB::raw("'{$needed_amount}' as item_number")
-                )
-                ->where('id', $stock_id)
-                ->groupBy('id', 'shop_name')
-                ->get();
+        $stocks = [];
     
-            $stocks[] = $results;
+        // Retrieve the details for each shop
+        foreach ($all_needs_stocks as $shopName => $neededStocks) {
+            foreach ($neededStocks as $stock_id => $needed_amount) {
+                $result = DB::table('stocks')
+                    ->select('name',
+                        'shop_name',
+                        'price',
+                        'item_unit',
+                        DB::raw("'{$needed_amount}' as item_number")
+                    )
+                    ->where('id', $stock_id)
+                    ->first();
+    
+                $stocks[$shopName][] = $result;
+            }
         }
     
-        dd($stocks); // この行はデバッグのためだけにあると思われるので、最終的な実装では削除またはコメントアウトしてください。
+        dd($stocks); 
     
-        return view('list', ['dishes' => []]); // この行ではdishesを空の配列として返しています。必要に応じて修正してください。
+        return view('list', ['dishes' => []]);
     }
+    
 }
