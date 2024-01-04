63f31b6f9138c55d4c7c19098d27e63d2a94db2b
nagasaki-0225 <133938359+nagasaki-0225@users.noreply.github.com>
2024-01-05 00:56:48 +0900

    更新取り消し

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StockController;
 use App\Http\Controllers\UsersController;
-use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -11,31 +11,15 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/deleted', [App\Http\Controllers\UsersController::class, 'userDeleteComplete'])->name('after_user_delete');
 Route::get('/', function () {
-    return redirect('login');
-});
-
-
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
-
+    return view('welcome');
+ });
+ 
 Auth::routes();
 
 // Route::get('/ ページのurl ',[\App\Http\Controllers\TopController::class,'    '])->name('ルートの名前');
