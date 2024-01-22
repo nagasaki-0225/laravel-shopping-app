a4c312e5e52b4467f513193d1e3b0b996d3517dd
nagasaki-0225 <133938359+nagasaki-0225@users.noreply.github.com>
2024-01-22 13:55:27 +0900

    退会ページ

--- resources/views/my_page/userDelete.blade.php
+++ resources/views/my_page/userDelete.blade.php
@@ -1,1 +1,16 @@
+@extends('layouts.app')
+
+@section ('pagename') 退会 @endsection
+
+@section('pagecss')
+    <link rel="stylesheet" href="{{ asset('css/top.css') }}">
+@endsection
+
+@section('content')
 <h1>退会しました</h1>
+
+<a href="{{route('top')}}" class="example1">新規登録・ログイン画面へ</a>
+
+
+@endsection
+
