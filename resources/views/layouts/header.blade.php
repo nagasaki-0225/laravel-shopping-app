31b26bc27fe863857b98d02fc513fe691f99f1c7
nagasaki-0225 <133938359+nagasaki-0225@users.noreply.github.com>
2024-01-22 13:15:47 +0900

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,51 +1,59 @@
 <header>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top"> 
-        <div class="container">
+        <div class="container d-flex mb-3">
             <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
-            <a class="navbar-brand" href="{{ url('/') }}">
+            <div class="me-auto p-2">
+                <a class="navbar-brand" href="{{ url('/') }}">
                 {{ config('app.name', 'Laravel') }}
-            </a>
-            <div class="pc">
-                <a href="{{ route('dish.index') }}" class="example-btn">dish</a>
-                <a href="{{ route('list') }}" class="example-btn">list</a>
-                <a href="{{ route('stock') }}" class="example-btn">stock</a> 
-                <a href="{{ route('my_page.index') }}" class="example-btn">mypage</a>
-            </div>
-            <div button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
+                </a>
             </div>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav me-auto">
+            <div class="p-2">
+                <div class="pc">
+                    <a href="{{ route('dish.index') }}" class="example-btn">dish</a>
+                    <a href="{{ route('list') }}" class="example-btn">list</a>
+                    <a href="{{ route('stock') }}" class="example-btn">stock</a> 
+                    <a href="{{ route('my_page.index') }}" class="example-btn">mypage</a>
+                </div>
+            </div>
 
-                </ul>
+            <div class="p-2">
+                <div button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </div>
 
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ms-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                       
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
 
-                            <div class="dropdown-menu dropdown-menu-end text-center" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                        
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
+                                <div class="dropdown-menu dropdown-menu-end text-center" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
             </div>
         </div>
     </nav>
